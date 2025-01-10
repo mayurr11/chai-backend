7e3b6c77b1399fc64f02953c1db0e14fd72618c9
mayur11 <mayurchauhan1107@gmail.com>
2025-01-10 11:42:21 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -4,11 +4,28 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
-dotenv.config({ 
-    path: ".env" 
+dotenv.config({
+  path: ".env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log($`Server is running on port ${process.env.PORT}`)
+    });
+    app.on("error", (error) => {
+        console.log("Error: ", error);
+        throw new Error("Error connecting to database");
+    });
+  })
+  .catch(error => {
+    console.log("Error: ", error);
+    throw new Error("Error connecting to database");
+  })
+  .catch(error => {
+    console.log("Error: ", error);
+    throw new Error("Error connecting to database");
+  });
 
 /*
 import express from "express";
